b843cd1dac65d4546552834f88aaa798fbb6f279
marianneo812 <marianne368@gmail.com>
2024-05-20 20:18:25 +1000

    added Optuna Model Optimisation to model.ipynb file. Saved the optimisation as a csv. saved the optimised model as a joblib file.

--- create_database.py
+++ create_database.py
@@ -1,16 +1,14 @@
+import sqlite3
 import pandas as pd
-from sqlalchemy import create_engine
 
-# Load the dataset
+# Load the CSV data into a DataFrame
 df = pd.read_csv('heart_disease.csv')
 
-# Drop the unnecessary columns
-df = df.drop(columns=['AnyHealthcare', 'NoDocbcCost', 'Income'])
+# Connect to SQLite database (it will be created if it doesn't exist)
+conn = sqlite3.connect('heart_disease.db')
 
-# Create a connection to the SQLite database
-engine = create_engine('sqlite:///heart_disease.db')
+# Save DataFrame to the SQLite database
+df.to_sql('heart_disease', conn, if_exists='replace', index=False)
 
-# Write the data to the SQLite database
-df.to_sql('heart_disease_table', engine, if_exists='replace', index=False)
-
-print("Database and table created successfully with required columns.")
+# Close the connection
+conn.close()
